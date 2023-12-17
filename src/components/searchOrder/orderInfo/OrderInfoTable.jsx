1ad2cdae9273a4e4bb71940c599e10155bb946dd
dungnh <dungnh@vnpt-technology.vn>
2023-12-17 22:46:57 +0700

    track order

--- src/components/searchOrder/orderInfo/OrderInfoTable.jsx
+++ src/components/searchOrder/orderInfo/OrderInfoTable.jsx
@@ -1,12 +1,14 @@
+/* eslint-disable react/prop-types */
 import { Checkbox } from "antd";
 import StatusLabel from "../../statusLabel";
 
-const OrderInfoTable = () => {
+const OrderInfoTable = ({ orderInfo }) => {
   const options = [
     {
       label: "Tài liệu",
-      value: "documents",
+      value: "document",
       checked: true,
+      disabled: true,
     },
     {
       label: "Hàng hóa",
@@ -14,8 +16,33 @@ const OrderInfoTable = () => {
       disabled: true,
     },
   ];
+  const {
+    sender,
+    receiver,
+    senderPhone,
+    receiverPhone,
+    send_department,
+    receive_department,
+    status,
+    createdAt,
+    expectedDate,
+    weight,
+    COD,
+    price,
+    type,
+  } = orderInfo;
+  const formatCurrency = (value) => {
+    return new Intl.NumberFormat("de-DE", {
+      style: "currency",
+      currency: "VND",
+    }).format(value);
+  };
+  const formatTime = (value) => {
+    const date = new Date(value);
+    return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+  };
   return (
-    <div className="overflow-x-auto">
+    <div className="overflow-x-auto w-full">
       <table className="w-full overflow-hidden bg-white rounded-lg shadow-md table-auto">
         <thead className="bg-[#f5f6f9]">
           <tr>
@@ -36,36 +63,38 @@ const OrderInfoTable = () => {
         <tbody className="text-gray-700">
           <tr>
             <td className="px-6 py-3 border-gray-200">Họ và tên:</td>
-            <td className="px-6 py-3 border-b border-gray-200">
-              Nguyễn Hải Nam
-            </td>
+            <td className="px-6 py-3 border-b border-gray-200">{sender}</td>
             <td className="px-6 py-3 border-b border-r border-gray-200">
               Họ và tên:
             </td>
-            <td className="px-6 py-3 border-b border-gray-200">Lê Lung Linh</td>
+            <td className="px-6 py-3 border-b border-gray-200">{receiver}</td>
           </tr>
           <tr>
             <td className="px-6 py-3 border-b border-r border-gray-200">
               Điện thoại:
             </td>
-            <td className="px-6 py-3 border-b border-gray-200">123-456-7890</td>
+            <td className="px-6 py-3 border-b border-gray-200">
+              {senderPhone}
+            </td>
             <td className="px-6 py-3 border-b border-r border-gray-200">
               Điện thoại:
             </td>
-            <td className="px-6 py-3 border-b border-gray-200">987-654-3210</td>
+            <td className="px-6 py-3 border-b border-gray-200">
+              {receiverPhone}
+            </td>
           </tr>
           <tr>
             <td className="px-6 py-3 border-b border-r border-gray-200">
               Địa chỉ:
             </td>
             <td className="px-6 py-3 border-b border-gray-200">
-              Tăng Thiết Giáp, phường Cổ Nhuế, quận Bắc Từ Liêm, Hà Nội
+              {send_department?.address}
             </td>
             <td className="px-6 py-3 border-b border-r border-gray-200">
               Địa chỉ:
             </td>
             <td className="px-6 py-3 border-b border-gray-200">
-              Trần Hưng Đạo, quận Hà Đông, Hà Nội
+              {receive_department?.address}
             </td>
           </tr>
         </tbody>
@@ -92,50 +121,64 @@ const OrderInfoTable = () => {
           <tr>
             <td className="px-6 py-3 border-gray-200">Loại hàng gửi:</td>
             <td className="px-6 py-3 border-b border-gray-200">
-              <Checkbox.Group options={options} defaultValue={["documents"]} />
+              <Checkbox.Group options={options} defaultValue={[type]} />
             </td>
             <td className="px-6 py-3 border-b border-r border-gray-200">
-              Phí giao hàng:
+              Giá trị hàng hóa:
             </td>
-            <td className="px-6 py-3 border-b border-gray-200">27.000đ</td>
+            <td className="px-6 py-3 border-b border-gray-200">
+              {formatCurrency(price)}
+            </td>
           </tr>
           <tr>
             <td className="px-6 py-3 border-b border-r border-gray-200">
               Mã đơn hàng:
             </td>
             <td className="px-6 py-3 font-semibold border-b border-gray-200">
-              SPXVN03668440929B
+              {orderInfo?._id}
             </td>
             <td className="px-6 py-3 border-b border-r border-gray-200">
-              Phí trả hàng:
+              Cước:
             </td>
-            <td className="px-6 py-3 border-b border-gray-200">20.000đ</td>
+            <td className="px-6 py-3 border-b border-gray-200">
+              {formatCurrency(COD)}
+            </td>
           </tr>
           <tr>
             <td className="px-6 py-3 border-b border-r border-gray-200">
               Trạng thái hiện tại:
             </td>
             <td className="px-6 py-3 border-b border-gray-200">
-              <StatusLabel />
+              <StatusLabel status={status} />
             </td>
             <td className="px-6 py-3 border-b border-r border-gray-200">
               Tổng thu:
             </td>
-            <td className="px-6 py-3 border-b border-gray-200">47.000đ</td>
+            <td className="px-6 py-3 border-b border-gray-200">
+              {formatCurrency(price + COD)}
+            </td>
           </tr>
           <tr>
             <td className="px-6 py-3 border-b border-r border-gray-200">
               Thời gian gửi hàng:
             </td>
             <td className="px-6 py-3 border-b border-gray-200">
-              08:45 25/11/2003
+              {formatTime(createdAt)}
+            </td>
+          </tr>
+          <tr>
+            <td className="px-6 py-3 border-b border-r border-gray-200">
+              Thời gian gửi hàng:
+            </td>
+            <td className="px-6 py-3 border-b border-gray-200">
+              {formatTime(expectedDate)}
             </td>
           </tr>
           <tr>
             <td className="px-6 py-3 border-b border-r border-gray-200">
               Cân nặng:
             </td>
-            <td className="px-6 py-3 border-b border-gray-200">10KG</td>
+            <td className="px-6 py-3 border-b border-gray-200">{weight} kg</td>
           </tr>
         </tbody>
       </table>
