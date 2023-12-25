6b3741f289388afbf0892b628c03e6e6e0728f8a
dat02092k1 <ngodat02092001@gmail.com>
2023-12-25 17:45:32 +0700

    "List orders by dep"

---
+++ src/pages/DashBoard/boss/ManagePointOrders.jsx
@@ -0,0 +1,19 @@
+import { Divider, Typography } from "antd";
+import CollectionPointTable from "../../../components/BossPage/CollectionPointTable";
+import PointList from "../../../components/BossPage/PointList";
+
+const ManagePointOrders = () => {
+  return (
+    <div className="w-full h-full">
+      <Typography.Title className="mb-0" level={1}>
+        Quản lý điểm kho
+      </Typography.Title>
+      <Divider />
+      <div className="w-full">
+        <PointList />
+      </div>
+    </div>
+  );
+};
+
+export default ManagePointOrders;
