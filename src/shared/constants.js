9402b3c15302d986f50e874442461cddd9fe4511
quangchinh186 <quangchinh1122@gmail.com>
2023-12-29 15:18:34 +0700

    tab icon, name, baseUrl

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,5 +1,6 @@
 const UtilConstants = {
-    baseUrl: 'http://localhost:3000/v1/api', // API base url 
+    baseUrl: 'https://magic-post-server-n25w.onrender.com/v1/api', // API base url
+    //baseUrl: 'http://localhost:3000/v1/api', // API base url 
 }
 
 export default UtilConstants;    
